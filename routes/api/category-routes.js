e39f638b0d71e178887757f2a9564318c3208832
EllieWalsch <elliewalsch4@gmail.com>
2023-01-19 13:39:04 -0600

    Find one category by id including its products

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,10 +15,24 @@ router.get('/', async (req, res) => {
   // Find all categories including their associated products
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  } 
+  // Find one category by its `id` value including its accociated products
 });
+ 
 
 router.post('/', (req, res) => {
   // create a new category
