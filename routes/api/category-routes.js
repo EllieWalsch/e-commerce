c96449f25fa350b58a8c5941009ea1bfd493702a
EllieWalsch <elliewalsch4@gmail.com>
2023-01-19 14:42:42 -0600

    Update a category by its id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,8 +46,23 @@ router.post('/', async (req, res) => {
   // Create a new category
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // Update a category by its id value
 });
 
 router.delete('/:id', (req, res) => {
