0e12161669e13d3e3af1163f0a14ee51edd041bb
EllieWalsch <elliewalsch4@gmail.com>
2023-01-20 12:38:31 -0600

    Find one product by id including category and tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -17,9 +17,22 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Tag }, { model: Category }]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  } 
+  // Find one product by its `id` value including its accociated category and tags
 });
 
 // create new product
