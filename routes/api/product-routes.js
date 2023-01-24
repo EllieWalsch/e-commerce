8a1549d66717f0c5d284858576b24cab427b5d9f
EllieWalsch <elliewalsch4@gmail.com>
2023-01-23 22:10:18 -0600

    Delete a product by its id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -109,8 +109,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: parseInt(req.params.id)
+      },
+    });
+    
+    res.status(200).json(productData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // Delete a product by its id value
 });
 
 module.exports = router;
